ca98264d8bea53759faf4b0a825c643d590b96a1
pavelchernov <p.chernov@city-mobil.ru>
2021-12-29 15:44:40 +0300

    добавил фрагмент с гербами и открывание этого фрагмента начал делать ландшафтную ориентацию

--- app/src/main/java/com/example/fragmentapp/CitiesFragment.java
+++ app/src/main/java/com/example/fragmentapp/CitiesFragment.java
@@ -1,9 +1,12 @@
 package com.example.fragmentapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,25 +22,54 @@ public class CitiesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cities, container, false);
     }
 
     @Override
     public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         initList(view);
     }
 
     private void initList(View view) {
         LinearLayout layoutView = (LinearLayout) view;
         String[] cities = getResources().getStringArray(R.array.cities);
-        for (String city : cities) {
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i];
             TextView tv = new TextView(getContext());
             tv.setText(city);
             tv.setTextSize(30);
             layoutView.addView(tv);
+
+            final int position = i;
+            tv.setOnClickListener(v -> {
+                showCoatOfArms(position);
+            });
         }
+    }
+
+    private void showCoatOfArms(int index) {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            showLandCoatOfArms(index);
+        } else {
+            showPortCoatOfArms(index);
+        }
+
+    }
+
+    private void showPortCoatOfArms(int index) {
+        CoatOfArmsFragment coatOfArmsFragment = CoatOfArmsFragment.newInstance(index);
+            //чтобы запустить фрагмент из фрагмента нужно вызвать getSupportFragmentManager() через requireActivity()
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack("");
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction
+                .add(R.id.fragment_container, coatOfArmsFragment)
+                .commit();
+    }
+
+    private void showLandCoatOfArms(int index) {
+
     }
 }
